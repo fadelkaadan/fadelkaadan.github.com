0a751c1b206e001fdb3dee5116f315feb091bbbd
Fadelkaadan <fadel95kaadan@gmail.com>
2016-05-07 23:31:47 +0200

    make story page

---
+++ js/javascript.js
@@ -0,0 +1,64 @@
+$(window).ready(function() {
+    $('.navbar-close').click(function(){
+      $('.nav').animate({
+      left: "-420"
+      });
+    });
+    $('.navbar-toggler').click(function(){
+      $('.nav').animate({
+      left: "0"
+      });
+    });
+
+    //arrowDown
+    $('.arrowDown').click(function(){
+      $('html,body').animate({
+     scrollTop: $('.mainVsco').offset().top
+      });
+    });
+
+    //age
+    var today = new Date();
+    var year = today.getFullYear();
+    var age = year - 1995;
+
+    //shuffle
+    $('.picThr').click(function(){
+      $('.picThr').slideUp("fast");
+      $('.picThr').animate({"margin":"90px 0 0 35%", "z-index":"1"});
+      $('.picThr').slideDown("fast");
+      $('.picTwo').animate({"margin":"35px 0 0 20%", "z-index":"4"});
+      $('.picOne').animate({"margin":"55px 0 0 25%", "z-index":"3"});
+      $('.picMore').animate({"margin":"75px 0 0 30%", "z-index":"2"});
+  });
+    $('.picTwo').click(function(){
+      $('.picTwo').slideUp("fast");
+      $('.picTwo').animate({"margin":"90px 0 0 35%", "z-index":"1"});
+      $('.picTwo').slideDown("fast");
+      $('.picOne').animate({"margin":"35px 0 0 20%", "z-index":"4"});
+      $('.picThr').animate({"margin":"75px 0 0 30%", "z-index":"2"});
+      $('.picMore').animate({"margin":"55px 0 0 25%", "z-index":"3"});
+  });
+    $('.picOne').click(function(){
+      $('.picOne').slideUp("fast");
+      $('.picOne').animate({"margin":"90px 0 0 35%", "z-index":"1"});
+      $('.picOne').slideDown("fast");
+      $('.picThr').animate({"margin":"55px 0 0 25%", "z-index":"3"});
+      $('.picTwo').animate({"margin":"75px 0 0 30%", "z-index":"2"});
+      $('.picMore').animate({"margin":"35px 0 0 20%", "z-index":"4"});
+  });
+  $('.picMore').click(function(){
+    $('.picMore').slideUp("fast");
+    $('.picMore').animate({"margin":"90px 0 0 35%", "z-index":"1"});
+    $('.picMore').slideDown("fast");
+    $('.picThr').animate({"margin":"35px 0 0 20%", "z-index":"4"});
+    $('.picTwo').animate({"margin":"55px 0 0 25%", "z-index":"3"});
+    $('.picOne').animate({"margin":"75px 0 0 30%", "z-index":"2"});
+});
+
+  //welcome
+  //$('.welcome').animate({"left":"20%"},"slow").fadeIn('slow').fadeOut('slow');
+
+  //under construction
+  $('.const').delay(1500).fadeOut();
+});
