5ef4ee27d0c5dd280fb9e4768670e1b891dcbd85
fadelkaadan <fadel95kaadan@gmail.com>
2018-02-06 22:49:08 +0100

    implemented the maximize btn on the terminal

--- js/terminal_script.js
+++ js/terminal_script.js
@@ -1,1 +1,38 @@
-//bla
+$(window).ready(function() {
+  $('#min').click(function() {
+    for (var i = 0; i < 70; i++) {
+      $('.box').animate({
+        'width': '-=2%',
+        'height': '-=2%',
+        'top': '+=1%',
+        'left': '+=1%'
+      }, 5);
+    }
+    $('.btns').css({'display':'none'});
+  });
+
+  var isClicked = false;
+  $('#max').click(function() {
+    if (isClicked == false) {
+      isClicked = true;
+      for (var i = 0; i < 5; i++) {
+        $('.box').animate({
+          'width': '+=2%',
+          'height': '+=2%',
+          'margin': '-=1% -=1% 0 -=1%'
+        }, 10)
+      }
+    } else {
+      isClicked = false;
+      for (var i = 0; i < 5; i++) {
+        $('.box').animate({
+          'width': '-=2%',
+          'height': '-=2%',
+          'margin': '+=1% +=1% 0 +=1%'
+        }, 10)
+      }
+    }
+
+
+  })
+})
