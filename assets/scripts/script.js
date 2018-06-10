83956ac6e4bcc5b3afc9b6e621e451cf90b4213f
fadelkaadan <fadel95kaadan@gmail.com>
2018-06-10 19:35:52 +0200

    fix falcon heavy

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,3 +3,6 @@ let falcon = document.querySelector('.falcon__logo')
 btn.addEventListener('click', () => {
     falcon.classList.add('animate');
 })
+btn.addEventListener('touchstart', () => {
+    falcon.classList.add('animate');
+})
