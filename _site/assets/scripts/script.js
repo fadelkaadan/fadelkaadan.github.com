b67cbd6792b8033079a0fe3ced8ce867d50b4fb1
fadelkaadan <fadel95kaadan@gmail.com>
2018-06-13 04:07:45 +0200

    fix hover on phones

--- _site/assets/scripts/script.js
+++ _site/assets/scripts/script.js
@@ -1,5 +1,9 @@
 let btn = document.querySelector('.btn');
-let falcon = document.querySelector('.falcon__logo')
+let falcon = document.querySelector('.falcon__logo');
+let deck = document.querySelectorAll('.deck__card');
+let card3 = document.querySelectorAll('.deck__card-3');
+var cardAfter = document.createElement('style');
+
 btn.addEventListener('click', (event) => {
     event.preventDefault;
     falcon.classList.add('animate');
@@ -8,3 +12,19 @@ btn.addEventListener('touchstart', (event) => {
     event.preventDefault;
     falcon.classList.add('animate');
 })
+
+deck.forEach(function(card) {
+    card.addEventListener('click', () => {
+        if (!isAnimated(card)) {
+            card.classList.add('deck__card--animated');
+        } else if (isAnimated(card)) {
+            card.classList.remove('deck__card--animated');
+        }
+    });
+});
+function isAnimated(item) {
+    if (item.classList.contains('deck__card--animated')) {
+        return true;
+    }
+    return false;
+}
