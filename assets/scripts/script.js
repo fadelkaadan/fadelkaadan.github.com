7dbb8d852391ed19dce548e619b5b5d53305305e
fadelkaadan <fadel95kaadan@gmail.com>
2018-07-18 14:15:09 +0200

    Site updated at 2018-07-18 12:15:09 UTC

---
+++ assets/scripts/script.js
@@ -0,0 +1,31 @@
+let btn = document.querySelector('.falcon--btn');
+let falcon = document.querySelector('.falcon--icon');
+let deck = document.querySelectorAll('.deck__card');
+btn.addEventListener('click', animate);
+
+deck.forEach(function(card) {
+    card.addEventListener('click', () => {
+        if (!isAnimated(card)) {
+            card.classList.add('deck__card--animated');
+        } else if (isAnimated(card)) {
+            card.classList.remove('deck__card--animated');
+        }
+    });
+});
+function isAnimated(item) {
+    if (item.classList.contains('deck__card--animated')) {
+        return true;
+    }
+    return false;
+}
+function animate() {
+    if (!falcon.classList.contains('launch')) {
+        if (falcon.classList.contains('land')) {
+            falcon.classList.remove('land');
+        }
+        falcon.classList.add('launch');
+    } else {
+        falcon.classList.remove('launch');
+        falcon.classList.add('land');
+    }
+}
